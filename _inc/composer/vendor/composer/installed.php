28a482707908e938693556e8bdafddc58a4e6944
Adam Zjavka <adam.zjavka@gmail.com>
2021-11-29 17:24:05 +0100

    upload of the whole web

---
+++ _inc/composer/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '238333bde690e9f88e60c8e532f84e44f060b501',
+    'name' => 'adam/composer',
+  ),
+  'versions' => 
+  array (
+    'adam/composer' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '238333bde690e9f88e60c8e532f84e44f060b501',
+    ),
+    'facebook/graph-sdk' => 
+    array (
+      'pretty_version' => '5.7.0',
+      'version' => '5.7.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2d8250638b33d73e7a87add65f47fabf91f8ad9b',
+    ),
+  ),
+);
